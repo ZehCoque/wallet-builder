a4641c023868b8ac0b72e35bfa3a1367f6986865
ZehCoque <jose.coque@hotmail.com>
2021-06-01 14:34:15 -0300

    feat: token for authentication

--- token/token.js
+++ token/token.js
@@ -13,19 +13,22 @@ async function getExistentToken(username, password) {
     },
   };
 
-  await dynamoDb.get(params, (error, data) => {
+  var p = new Promise((resolve, reject) => {
+    dynamoDb.get(params, (error, res) => {
 
-    if (error) {
-      console.error(error);
-      throw error;
-    }
+      if (error) {
+        console.error(error);
+        reject(error);
+      }
 
-    const existent = data.filter(
-      (item) => CryptoHelper.decrypt(item.password) === password
-    );
-    if (existent && existent[0]) return existent[0].token;
+      if (!res.Item) resolve(false)
+      resolve(res.Item);
+      
+    });
+  })
 
-  });
+  return p
+
 }
 
 module.exports.createToken = async (event, context, callback) => {
@@ -36,31 +39,22 @@ module.exports.createToken = async (event, context, callback) => {
       let username = body.username;
       let password = body.password;
 
-      const existentToken = await getExistentToken(
-        username,
-        password
-      );
+      const existentEntry = await getExistentToken(username,password);
 
-      if (existentToken) {
-        console.log("Existent token for user:", existentToken);
-
-        const response = {
-          statusCode: 201,
-          body: JSON.stringify({token: existentToken}),
-        };
-        callback(null, response);
+      if (existentEntry.password) {
+        if (CryptoHelper.decrypt(existentEntry.password) === password) {
+          console.log("Existent token for user:", existentEntry.token);
+  
+          const response = {
+            statusCode: 201,
+            body: JSON.stringify({token: existentEntry.token}),
+          };
+          return callback(null, response);
+        } 
       }
 
       const encryptedPass = CryptoHelper.encrypt(password);
       const token = uuid.v4();
-
-      const data = {
-        username: username,
-        password: encryptedPass,
-        token: token,
-      };
-
-      console.log("Inserting new credentials: ", data);
 
       const params = {
         TableName: tableName,
@@ -72,25 +66,32 @@ module.exports.createToken = async (event, context, callback) => {
       };
 
       return dynamoDb.put(params, (error) => {
-        console.log(params)
         if (error) {
           console.error(error);
-          callback(null, JSON.stringify({
-            statusCode: error.statusCode || 501,
-            headers: { 'Content-Type': 'text/plain' },
-            body: 'Couldn\'t create token.',
-          }));
+
+          const response = {
+            statusCode: 201,
+            body: JSON.stringify({error: 'Couldn\'t create token.'}),
+          };
+
+          return callback(null, response);
+
         }
         
         const response = {
           statusCode: 201,
           body: JSON.stringify({token: token}),
         };
-        callback(null, response);
+        return callback(null, response);
       }).promise();
+      
 
     } catch (error) {
       console.log("Error:", error);
-      res.status(500).json();
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({error: error}),
+      };
+      return callback(null, response);
     }
 }
