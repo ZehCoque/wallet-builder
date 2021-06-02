1f7a4ea55c9a997c831890be45cf2ad6daf8669a
ZehCoque <jose.coque@hotmail.com>
2021-06-02 19:50:52 -0300

    getTickersFromDynamo

--- tickerTracker.js
+++ tickerTracker.js
@@ -3,6 +3,29 @@ const lambda = new aws.Lambda({
   region: 'sa-east-1'
 });
 
+const tableName = process.env.DYNAMODB_TABLE + "-TICKER";
+
+async function getTickersFromDynamo() {
+
+  const params = {
+    TableName: tableName,
+    ProjectionExpression: 'ticker'
+  };
+
+  var p = new Promise((resolve, reject) => {
+    dynamoDb.scan(params, (error, res) => {
+      if (error) {
+        console.error('DynamoDB error: ' + error);
+        reject(error);
+      }   
+      resolve(res.Items);
+    });
+  })
+
+  return p;
+
+}
+
 module.exports.tickerTracker = (event, context, callback) => {
     const username = event.Records[0].dynamodb.Keys.username.S;
     const payload = {queryStringParameters: {username: username}};
@@ -17,9 +40,20 @@ module.exports.tickerTracker = (event, context, callback) => {
           console.log(error);
           return error;
         }
-        const result = JSON.parse(data);
-        const body = result.body;
+        console.log(data)
+        const body = JSON.parse(data);
 
+        console.log(body);
 
-      });
+      // body.forEach(institution => {
+      //   institution.stockHistory.forEach(operation => {
+      //       console.log(operation.code)
+      //   });
+      // });
+
+    });
+
+    const dynamoTickers = await getTickersFromDynamo();
+
+    console.log(dynamoTickers);
 }
