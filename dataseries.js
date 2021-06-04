0280086e3808d67951cae25ae4c34ee85f15ce4a
ZehCoque <jose.coque@hotmail.com>
2021-06-04 00:55:55 -0300

    feat: starting dataseries update function

--- dataseries.js
+++ dataseries.js
@@ -1,113 +1,18 @@
-'use strict';
-
 const axios = require('axios');
 
-const headers = {
-  "x-rapidapi-key": process.env.API_KEY,
-	"x-rapidapi-host": process.env.API_HOST,
-	"useQueryString": true
-}
+const tableName = process.env.DYNAMODB_TABLE + "-TICKER";
+const API_URI = process.env.STOCKS_API;
+const API_KEY = process.env.API_KEY;
 
-function candlestick(timeseries) {
+function getChartData(outputSize){
 
-  // desired format
-  // series: {
-  //   x: timestamp,
-  //   y: [open,high,low,close]
-  // }
-
-  let chartData = [];
-
-  let promise = new Promise((resolve, reject) => {
-    
-      if (!timeseries) reject('No json object found!');   
-
-      for (var key in timeseries){
-
-        let open = timeseries[key]['1. open'];
-        let high = timeseries[key]['2. high'];
-        let low = timeseries[key]['3. low'];
-        let close = timeseries[key]['4. close'];
-
-        chartData = chartData.concat([{
-          x: new Date(key),
-          y: [open,high,low,close]
-        }])
-      }
-
-      resolve(chartData);
-
-  })
-
-  return promise;
-
-}
-
-function line(timeseries) {
-
-  // desired format
-  // series: {
-  //   x: timestamp,
-  //   y: [open,high,low,close]
-  // }
-
-  let chartData = [];
-
-  let promise = new Promise((resolve, reject) => {
-    
-      if (!timeseries) reject('No json object found!');   
-
-      for (var key in timeseries){
-
-        let close = timeseries[key]['4. close'];
-
-        chartData = chartData.concat([{
-          x: new Date(key),
-          y: close
-        }])
-      }
-
-      resolve(chartData);
-
-  })
-
-  return promise;
-
-}
-
-function aggregate(allChartData, quantities){
-
-  let promise = new Promise((resolve, reject) => {
-
-    if (!allChartData) reject('allChartData missing!');
-    if (!quantities) reject('quantities missing!');
-
-    let aggr = [];
-
-    for (var i = 0; i < allChartData[0].length; i++){
-      let y = 0;
-  
-      for (var key = 0; key < allChartData.length; key++){
-  
-          y += Number(allChartData[key][i].y) * quantities[key];
-        }
-  
-        aggr = aggr.concat([{
-              x: allChartData[0][i].x,
-              y: y
-            }])
-    }
-    
-  
-  resolve(aggr);
-
-  });
-
-  return promise;
-
-}
-
-function getChartData(URI, params){
+  const URI = API_URI;
+  const params = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": ticker,
+    "outputsize": outputSize,
+    "apikey": API_KEY
+  }
 
   let promise = new Promise((resolve, reject) => {
     axios.get(URI,{params: params, headers: headers})
@@ -122,15 +27,6 @@ function getChartData(URI, params){
       // "5. volume": string
       // }
   
-      line(response.data["Time Series (Daily)"]).then((chartData) => {
-   
-        resolve(chartData);
-  
-      })
-      .catch(error => {
-        console.log('candlestick function failed\n', error);
-        reject(error);
-      })
   
     })
     .catch(error => {
@@ -143,46 +39,35 @@ function getChartData(URI, params){
 
 }
 
-module.exports.getDataseries = async (event) => {
+function getDataFromDynamo() {
 
-  let URI = process.env.STOCKS_API
+  const params = {
+    TableName: tableName,
+  };
 
-  let tickers = ['VALE3.SA', 'TSLA34.SA', 'WEGE3.SA', 'IRDM11.SA'];
-  let quantities = [100, 50, 134, 243];
+  var p = new Promise((resolve, reject) => {
+    dynamoDb.scan(params, (error, res) => {
+      if (error) {
+        console.error('DynamoDB error: ' + error);
+        reject(error);
+      }
 
-  let promiseArray = [];
+      if (!res || !res.Items) reject('No tickers retrieved from DB');
 
-  await tickers.forEach(ticker => {
-
-    const params = {
-      "function": "TIME_SERIES_DAILY",
-      "symbol": ticker,
-      "outputsize": "compact",
-      "datatype": "json"
-    }
-
-    promiseArray.push(getChartData(URI, params));
-
-  }) 
-
-  return Promise.all(promiseArray).then((allChartData) => {
-
-    return aggregate(allChartData, quantities).then((results) => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(
-          {
-            results: results
-          },
-          null,
-          2
-        ),
-      };
-    })
-
-
+      resolve(res.Items);
+    });
   })
 
+  return p;
+
+}
+
+module.exports.dataseries = (event, context, callback) => {
+
+
+  getDataFromDynamo().then(data => {
+    console.log(data)
+  })
   
 
 };
