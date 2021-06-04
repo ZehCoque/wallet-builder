50dee3ff12dd58066b56d8190099c88e4be888aa
ZehCoque <jose.coque@hotmail.com>
2021-06-04 00:55:25 -0300

    fix: response text improvement

--- cei.js
+++ cei.js
@@ -20,7 +20,7 @@ async function getUsernameAndPassword(username) {
         console.error(error);
         reject(error);
       }   
-      if (!res || !res.Item) reject('Invalid username.')
+      if (!res || !res.Item) reject('Invalid or unregistered username.')
       resolve(res.Item.password);
     });
   })
